9fb2c6359981ab1720098d795d1835d2ad926d25
vladbossss <vlad.manda@cnimslatina.ro>
2025-06-11 15:03:28 +0300

    Update skinbid_bot_selenium.py

--- skinbid_bot_selenium.py
+++ skinbid_bot_selenium.py
@@ -1,4 +1,4 @@
-rom selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
